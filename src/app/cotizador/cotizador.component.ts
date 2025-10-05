2020b04d7d14c3c47f24c69471115ebac44d294c
JULIO DAVID MELGAR BARILLAS <jmelgarb4@miumg.edu.gt>
2025-10-04 23:07:28 -0600

    Listas de tablas CATALAGO, Nuevo diseno y version final

--- src/app/cotizador/cotizador.component.ts
+++ src/app/cotizador/cotizador.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
+
+
 @Component({
   selector: 'app-cotizador',
   standalone: true,
@@ -10,31 +12,64 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './cotizador.component.html',
   styleUrls: ['./cotizador.component.css']
 })
+
+
+
 export class CotizadorComponent {
   data = {
-    paisDestino: 'GT',
-    peso: 2.5,
-    alto: 20,
-    largo: 30,
-    ancho: 15,
-    clienteId: 2
+    paisDestino: null,
+    peso: null,
+    alto: null,
+    largo: null,
+    ancho: null,
+    clienteId: null
   };
+
 
   resultado: any = null;
   loading = false;
   error?: string;
 
+  paises: any[] = [];
+  clientes: any[] = [];
+  regiones: any[] = [];
+
   constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.cargarListas();
+  }
+
+ cargarListas() {
+    this.http.get('http://localhost:8080/api/paises').subscribe((res: any) => {
+      this.paises = res;
+    });
+
+    this.http.get('http://localhost:8080/api/clientes').subscribe((res: any) => {
+          this.clientes = res;
+   });
+
+    this.http.get('http://localhost:8080/api/regiones').subscribe((res: any) => {
+          this.regiones = res;
+        });
+      }
+
+
 
   cotizar() {
     this.loading = true;
     this.error = undefined;
     this.resultado = null;
 
-    this.http.post('http://localhost:8080/api/cotizar', this.data)
-      .subscribe({
-        next: (res) => { this.resultado = res; this.loading = false; },
-        error: (err) => { this.error = err?.error?.message || 'Error al cotizar'; this.loading = false; }
+    this.http.post('http://localhost:8080/api/cotizar', this.data).subscribe({
+        next: (res) => {
+           this.resultado = res; 
+           this.loading = false; 
+          },
+        error: (err) => { 
+          this.error = err?.error?.message || 'Error al cotizar'; 
+          this.loading = false; 
+        }
       });
   }
 }
